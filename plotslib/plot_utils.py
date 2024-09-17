f8f41b98b36cc8c983ed75a3b3d76d559471c35e
jonas <jrya@itu.dk>
2024-09-17 14:37:01 +0200

    add guide to lib file

--- plotslib/plot_utils.py
+++ plotslib/plot_utils.py
@@ -1,7 +1,14 @@
 import os
-import datetime
 
-# cwd = current working directory
+# call it in you file like this: 
+    # timeNowRaw = datetime.datetime.now()
+    # plotsDir = getPlotsDirectory(timeNowRaw, os.getcwd())
+
+# and do this in the import, where  '../../..' is whatever path that leads to repository's root directory:
+    # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+    # from plotslib.plot_utils import getPlotsDirectory
+
+# cwd = current working directory, time is datetime.datetime.now()
 def getPlotsDirectory(time, cwd):
     resDir = cwd + '/results/'
     resDateDir = resDir + str(time.date())
