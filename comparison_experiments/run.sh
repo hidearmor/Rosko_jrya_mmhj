77cc8c02679f9d2c5c0779133c2ba35665db3973
smollsies <mmhj@itu.dk>
2024-09-23 12:47:26 +0200

    comp experiments only with Rosko

--- comparison_experiments/run.sh
+++ comparison_experiments/run.sh
@@ -2,30 +2,29 @@
 
 # export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9";
 
-x=$PWD
-sudo cp mkl_sparse_gemm.cpp /opt/intel/oneapi/mkl/2021.1.1/examples/sycl/spblas
-cd /opt/intel/oneapi/mkl/2021.1.1/examples/sycl
-make sointel64 examples="spblas/mkl_sparse_gemm" sycl_devices=cpu
-cp _results/intel64_so_tbb/spblas/mkl_sparse_gemm.out $x
-cd $x
-
-
-
-#!/bin/bash
-
+# x=$PWD
+# sudo cp mkl_sparse_gemm.cpp /opt/intel/oneapi/mkl/2021.1.1/examples/sycl/spblas
+# cd /opt/intel/oneapi/mkl/2021.1.1/examples/sycl
+# make sointel64 examples="spblas/mkl_sparse_gemm" sycl_devices=cpu
+# cp _results/intel64_so_tbb/spblas/mkl_sparse_gemm.out $x
+# cd $x
 
 
 
 make;
 
-echo "algo,p,M,K,N,sp,time" >> results
+filename="results"
 
+echo "algo,p,sp,M,K,N,time,ntrials" >> $filename
 
-
-for xx in 70 75 80 85 90 95 98 99;
-do
-	./rosko_sgemm_test 40 4 288 2208 192 $xx
+for algo in rosko;
+do 
+	for sp in 70 75 80 85 90 95 98 99;
+	do
+		./rosko_sgemm_test 1000 1000 1000 8 $sp 10 $algo $filename
+	done
 done
+
 
 
 
