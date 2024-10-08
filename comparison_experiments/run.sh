d33dc376e7a4d36c7beb5407b2e25115694589da
jonas <jrya@itu.dk>
2024-10-08 14:14:57 +0200

    run.sh file has setup for results folders etc

--- comparison_experiments/run.sh
+++ comparison_experiments/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+x=$PWD
+cd ../CAKE_on_CPU;
+source env.sh;
+cd ..;
+source env.sh;
+cd $x;
+
+echo $ROSKO_HOME;
+echo $CAKE_HOME;
+
 # export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9";
 
 # x=$PWD
@@ -13,21 +23,53 @@
 
 make;
 
-filename="results"
+# filename="results" #Mayas imp
 
-echo "algo,p,sp,M,K,N,time,ntrials" >> $filename
+# check if results exists and if it does, move it
+FILE="results_comp"
+
+# Check if the results file already exists
+if [ -f "$FILE" ]; then
+	rm -f $FILE
+    echo "File '$FILE' exists. Removing..."
+else
+    echo "File '$FILE' does not exist."
+fi
+
+
+echo "algo,p,sp,M,K,N,time,ntrials" >> $FILE
+
+declare -i runs=2
+declare -i n=1000
+declare -i cores=8
 
 # algo might not be a relevenat parameter
 
 for sp in 70 75 80 85 90 95 98 99;
 do
-	./rosko_sgemm_test 1000 1000 1000 8 $sp 5 rosko $filename
-	./naive_mm_test 1000 1000 1000 $sp 5 naive $filename
+	./rosko_sgemm_test 	$n $n $n $cores $sp 5 rosko $FILE
+	./naive_mm_test 	$n $n $n $sp 5 naive $FILE
 done
 
+### PLOTS PART ####
 
+PYTHON_SCRIPT_PATH="$ROSKO_HOME/plotslib/plot_utils.py"
+FUNCTION_NAME="getPlotsDirectory"
+cwd=$PWD
+# Call the Python script with the function name as an argument
+output=$(python3 "$PYTHON_SCRIPT_PATH" "$FUNCTION_NAME" "$cwd")
 
+# Read the output values
+path=$(echo "$output" | sed -n '1p')
+time=$(echo "$output" | sed -n '2p')
+underscore="_"
 
+cp $FILE $path$time$underscore$FILE
 
+# python3 plots.py
 
+commit_hash=$(git rev-parse HEAD)
+logName="commit_hash.txt"
+echo "$commit_hash" > $path$time$underscore$logName
 
+#####################
