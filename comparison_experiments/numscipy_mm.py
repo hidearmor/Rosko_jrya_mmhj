8682e30bd6060a4754adb59f0a15fa9eb2b1d726
jonas <jrya@itu.dk>
2024-10-25 11:41:05 +0200

    sci-py diagonal format horserace 01

--- comparison_experiments/numscipy_mm.py
+++ comparison_experiments/numscipy_mm.py
@@ -11,49 +11,44 @@ def print_matrices(sparse_matrix, dense_matrix, result):
     print("\nDense Matrix:\n", dense_matrix.round(2))
     print("\nResult of Sparse x Dense Multiplication:\n", result.round(2))
 
-# does NOT work properly
 def diagonals_with_density(N, d):
-    # 1. Calculate the number of non-zero elements (nnz) based on the density
-    total_elements = N * N
-    nnz = int(d * total_elements)
-    
-    # 2. Estimate the number of diagonals
-    # We want an uneven number of diagonals that is close to the nnz we need
-    num_diagonals = 2 * (nnz // N) + 1  # Closest odd number of diagonals
-    
+    nnz = int(d * (N * N))
+    nnz_exact = 0
+    acc1 = 0
+    acc2 = N
+    counter = 2
+    num_diagonals = 1
+
+    while(acc2 < nnz):
+        acc1 = acc2
+        acc2 += 2*(N-counter)
+        counter += 2
+        num_diagonals += 2
+
+    if acc2-nnz <= nnz-acc1:
+        nnz_exact = acc2
+    else: 
+        num_diagonals += -2
+        nnz_exact = acc1
+
     # Ensure the number of diagonals does not exceed the matrix size
     if num_diagonals > 2 * (N - 1) + 1:
         num_diagonals = 2 * (N - 1) + 1  # Max number of diagonals is 2N-1
-    
-    # 3. Create the diagonal list centered around 0 (evenly distributed)
+
     half_diags = (num_diagonals - 1) // 2
     diags = list(range(-half_diags, half_diags + 1))
-    
-    # 4. Calculate the exact number of non-zero values based on the diagonals
-    nnz_exact = sum(N - abs(diag) for diag in diags)
-    exact_density = nnz_exact / total_elements
-    
+
+    exact_density = nnz_exact / (N * N)
     return diags, exact_density
 
-# def SparseDenseMM(M, K, N, sp):
+    # missing warmup runs
 def SparseDenseMM(N, d_org, format):
-    # 2. build random sparse matrix A with sparsity sp
-    # 3. build random dense matrix B
-    # 4. (maybe) convert to correct formats
-    # 5. if warmup: start time
-    # 6. perform multiplications
-    # 7. if warmup: end time and return it
-
-    diags, d = diagonals_with_density(N, d_org)
-    print(d, d_org)
-    
+    diags, d = diagonals_with_density(N, d_org)    
     dense_matrix = np.random.rand(N,N)
 
     rng = np.random.default_rng()
     sparse_matrix = None
-    # sparse_matrix = sp.sparse.random(N, N, density=d, random_state=rng, format='csr')
     if format == 'dia':
-        # diags = [0]
         data = rng.random((len(diags), N))
         sparse_matrix = sp.sparse.dia_array((data, diags), shape=(N, N))
     else:
@@ -63,27 +58,30 @@ def SparseDenseMM(N, d_org, format):
     result = sparse_matrix @ dense_matrix
     end = time.time()
 
-    # print(format + str(sparse_matrix.toarray().round(2)))
     return  end-start
 
-# formats = ['csr']
 formats = ['csr', 'bsr', 'coo', 'csc', 'dia']
+formats = ['csr', 'bsr', 'dia']
+headliner = 'format, N, density, time, runs, diagonals'
 file = open('format_horse_race.txt', 'a')
-file.write('format, N, density, time, runs')
+file.write(headliner)
+print(headliner)
 for format in formats:
-    res = 0
-    loops = 10
-    N = 4096
-    N = 100
-    d = 0.3
-    for i in range(0, loops):
-        SparseDenseMM(N, d, format)
-    for i in range(0, loops):
-        res += SparseDenseMM(N, d, format)
-    result = format + ', ' + str(N) + ', ' + str(d) + ', ' + str(round((res/(loops)), 7)) + ', ' + str(loops)
-    file.write('\n')
-    file.write(result)
-    print(result)
+    for d in [0.01, 0.05, 0.1, 0.2, 0.3, 0.5]:
+        res = 0
+        runs = 5
+        N = 4096
+        # N = 2048
+        # d = 0.08
+        diags, dens = diagonals_with_density(N, d)
+        for i in range(0, runs):
+            SparseDenseMM(N, d, format)
+        for i in range(0, runs):
+            res += SparseDenseMM(N, d, format)
+        result = format + ', ' + str(N) + ', ' + str(round(dens, 2)) + ', ' + str(round((res/(runs)), 7)) + ', ' + str(runs) + ', ' + str(len(diags))
+        file.write('\n')
+        file.write(result)
+        print(result)
 
 
 
