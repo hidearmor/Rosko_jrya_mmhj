38eb87784f62da6c8ad9ef6f95eea758d1e08ab8
smollsies <mmhj@itu.dk>
2024-11-03 20:07:17 +0100

    diagonal pattern generator + rand_sparse returns sparsity and not total nz

--- comparison_experiments/test_pattern.cpp
+++ comparison_experiments/test_pattern.cpp
@@ -11,26 +11,38 @@
 
 // int main(int argc, char *argv[]) {
 int main( int argc, char** argv ) {
-	int N = 10;
-	float sp = 90;
+	int N = 2048;
+	float sps[] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 97, 98, 99, 100};
+	int num_sps = sizeof(sps) / sizeof(sps[0]);
 	float* A = (float*) malloc(N * N * sizeof( float )); // initalizes with garbage value
+	int sp;
 
-    // float actual_sparsity = column_pattern_sparse(A, N, N, ((float) sp) / 100.0, -1); // init A with column pattern
-    // printf("column_pattern_sparse\nsparsity = %f\nactual_sparsity = %f\n", sp, (actual_sparsity*100));
+	for (int i = 0; i < num_sps; i++) {
+		sp = sps[i];
 
-	// float actual_sparsity = row_pattern_sparse(A, N, N, ((float) sp) / 100.0, -1); // init A with row pattern
-    // printf("row_pattern_sparse\nsparsity = %f\nactual_sparsity = %f\n", sp, (actual_sparsity*100));
+		// float actual_sparsity = column_pattern_sparse(A, N, N, ((float) sp) / 100.0, -1); // init A with column pattern
+		// printf("column_pattern_sparse\nsparsity = %f\nactual_sparsity = %f\n", sp, (actual_sparsity*100));
 
-	// int num_nnz = rand_sparse(A, N, N, ((float) sp) / 100.0); // init A with random uniform sparsity
-	// printf("rand_sparse\nnnz in A = %d\n", num_nnz);
-	// printf("input_sparsity = %f\n", ((float) sp) / 100.0);
-    // printf("actual_sparsity = %f\n", (100.0 - ((float) num_nnz / (float) (N*N)) * 100.0));
+		// float actual_sparsity = row_pattern_sparse(A, N, N, ((float) sp) / 100.0, -1); // init A with row pattern
+		// printf("row_pattern_sparse\nsparsity = %f\nactual_sparsity = %f\n", sp, (actual_sparsity*100));
 
-	float actual_sparisty = rand_sparse_teamOtter(A, N, N, ((float) sp) / 100.0); // init A with random uniform sparsity
-	printf("rand_sparse_teamOtter\n");
-	printf("input_sparsity = %f\n", ((float) sp) / 100.0);
-    printf("actual_sparsity = %f\n", (actual_sparisty));
+		float actual_sparsity = rand_sparse(A, N, N, ((float) sp) / 100.0); // init A with random uniform sparsity
+		printf("rand_sparse\n");
+		printf("input_sparsity = %f\n", ((float) sp) / 100.0);
+		printf("actual_sparsity = %f\n", actual_sparsity);
 
-	print_mat(A, N, N);
+		// float actual_sparisty = rand_sparse_teamOtter(A, N, N, ((float) sp) / 100.0); // init A with random uniform sparsity
+		// printf("rand_sparse_teamOtter\n");
+		// printf("input_sparsity = %f\n", ((float) sp) / 100.0);
+		// printf("actual_sparsity = %f\n", (actual_sparisty));
+
+		// test return of rand_sparse OG and 
+		// float actual_sparisty = diagonal_pattern_sparse(A, N, N, ((float) sp) / 100.0); // init A with random uniform sparsity
+		// printf("diagonal_pattern_sparse\n");
+		// printf("input_sparsity = %f\n", ((float) sp) / 100.0);
+		// printf("actual_sparsity = %f\n", (actual_sparisty));
+
+		// print_mat(A, N, N);
+	}
 }
 
--- include/util_sp.h
+++ include/util_sp.h
@@ -9,10 +9,12 @@ int run_tests_sparse_test();
 
 float rand_sparse_teamOtter(float* mat, int r, int c, float sparsity);
 
-int rand_sparse(float* mat, int r, int c, float sparsity);
+float rand_sparse(float* mat, int r, int c, float sparsity);
 
-float row_pattern_sparse(float* mat, int r, int c, float sparsity, int num_nnz_rows);
+float row_pattern_sparse(float* mat, int r, int c, float sparsity, int num_nz_rows);
 
-float column_pattern_sparse(float* mat, int r, int c, float sparsity, int num_nnz_cols);
+float column_pattern_sparse(float* mat, int r, int c, float sparsity, int num_nz_cols);
+
+float diagonal_pattern_sparse(float* mat, int r, int c, float sparsity);
 
 void rand_sparse_gaussian(float* mat, int r, int c, float mu, float sigma);
