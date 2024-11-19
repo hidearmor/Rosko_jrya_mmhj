8bf1249a14659209737e8c3d2a1de148386c2ebf
smollsies <mmhj@itu.dk>
2024-11-19 10:44:12 +0100

    exp setup for run_p_extensive

--- p-exploration/run_p_extensive.sh
+++ p-exploration/run_p_extensive.sh
@@ -41,19 +41,23 @@ if [ "$person" == "mmhj" ]; then
     
 	# declare -i trials=10
 	# declare -i warmups=10
-	declare -i trials=1
-	declare -i warmups=1
+	# declare -i trials=1
+	# declare -i warmups=1
+	declare -i trials=10
+	declare -i warmups=5
 	n_start=512
 	# n_end=2048
-	# n_end=4096
-	n_end=1024
+	n_end=4096
+	# n_end=1024
 	# n_end=10240
 	n_step=512
-	ps=(1 2 3 4)
+	# ps=(1 2 3 4)
+	ps=(1 2 3 4 5 6 7 8 9 10 15 20 35 40 45 50 60 70 80 90 100)
 	# ps=(1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 45 50 55 60 65 75 80 85 90 95 100)
 	# ps=(1 2 3 4 5 6 7 8 9 10 20 30 40 50 60 70 80 90 100 120 140 160 180 200 300 400 500 700 800 900 1000)
 	num_ps=${#ps[@]}
-	sparsity_values=(60 85 95 99)
+	# sparsity_values=(98 99 99.9)
+	sparsity_values=(50 60 70 85 95 97 98 99 99.9)
 	# sparsity_values=(20 30 40 50 60 70 80 85 90 95 97 98 99)
 	num_sparsity_values=${#sparsity_values[@]} # the number of sparsity values used in this experiment
 	measure="mm" # options: all, packing, mm
@@ -107,7 +111,6 @@ do
 		do
 			for sparsity_pattern in ${sparsity_patterns[@]};
 			do
-				# ./rosko_sgemm_test $n $n $n $p $sp $trials $warmups rosko $FILE $measure
 				./rosko_sgemm_test $n $n $n $p $sp $trials $warmups $sparsity_pattern rosko $FILE $measure
 			done
 		done
@@ -133,7 +136,7 @@ nameHype=$unscr$hyperthreading$unscr$person
 
 cp $FILE $path$time$unscr$FILE$unscr$measure$nameHype
 
-python3 plots_p.py $num_sparsity_patterns ${sparsity_pattern[@]} $num_sparsity_values ${sparsity_values[@]} $num_ps ${ps[@]} $n_start $n_end $n_step FILE $nameHype
+python3 plots_p_extensive.py $num_sparsity_patterns ${sparsity_patterns[@]} $num_sparsity_values ${sparsity_values[@]} $num_ps ${ps[@]} $n_start $n_end $n_step $FILE $nameHype
 
 commit_hash=$(git rev-parse HEAD)
 logName="commit_hash"
