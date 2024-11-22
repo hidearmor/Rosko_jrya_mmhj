d35f0368b635aec3cd2be3e62a81c442847bf168
smollsies <mmhj@itu.dk>
2024-11-22 10:24:05 +0100

    run plots only script

--- p-exploration/plots_part_only.sh
+++ p-exploration/plots_part_only.sh
@@ -10,9 +10,30 @@ cd $x;
 echo $ROSKO_HOME;
 echo $CAKE_HOME;
 
+# Check if the "person" argument is provided
+if [ -z "$1" ]; then
+    echo "Error: No argument provided. Please specify who is running the experiment (e.g., mmhj or jrya)."
+    exit 1  # Exit the script if no argument is provided
+fi
+
 # Set the "person" variable based on the first argument
 person=$1
+
 FILE="results_p_extensive"
+
+n_start=512
+n_end=4096
+# n_end=8192
+n_step=512
+ps=(6 10 14 20 40 43 46 50 75 100 125 150 175 200)
+# ps=(1 2 3 4 5 6 10 14 20 40 43 46 50 75 100 125 150 175 200)
+num_ps=${#ps[@]}
+sparsity_values=(20 30 40 50 60 70 80 85 90 95 97 98 99 99.9)
+num_sparsity_values=${#sparsity_values[@]} # the number of sparsity values used in this experiment
+measure="mm" # options: all, packing, mm
+sparsity_patterns=("random-uniform" "diagonal" "row-pattern" "column-pattern") # options: random-uniform, diagonal, row-pattern, column-pattern
+num_sparsity_patterns=${#sparsity_patterns[@]}
+
 hyperthreading=$($ROSKO_HOME/thesis_utils/hyperthreading.sh)
 
 
@@ -31,12 +52,7 @@ time=$(echo "$output" | sed -n '2p')
 unscr="_"
 nameHype=$unscr$hyperthreading$unscr$person
 
-cp $FILE $path$time$unscr$FILE$unscr$measure$nameHype
-
 python3 plots_p_extensive.py $num_sparsity_patterns ${sparsity_patterns[@]} $num_sparsity_values ${sparsity_values[@]} $num_ps ${ps[@]} $n_start $n_end $n_step $FILE $nameHype
 
-commit_hash=$(git rev-parse HEAD)
-logName="commit_hash"
-echo "$commit_hash" > $path$time$unscr$logName$unscr$FILE$nameHype
 
 #####################
